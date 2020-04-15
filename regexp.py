069651b8e4bb34b424956029a0dd838f2cc6e645
Eklera <lera.googlit@gmail.com>
2020-04-16 00:50:33 +0300

    Data output

--- regexp.py
+++ regexp.py
@@ -16,11 +16,12 @@ age_pattern = re.compile(r"\d+")
 tel_pattern = re.compile(r"\+{0,1}\d{11}")
 mail_pattern = re.compile(r"[\w!#$%&'*+-/=?^_`{|]+[^.@]@(\w+.)+\w+")
 
+
 def error_symbols(arg):
-    symbols = [".", "@", " "]
-
-
-
+    arg = re.sub(r"\.+", ".", arg)
+    arg = re.sub(r"@+", "@", arg)
+    arg = re.sub(r" ", "", arg)
+    return arg
 
 
 for line in txt:
@@ -35,6 +36,38 @@ for line in txt:
         continue
 
     name, age, tel, mail = line
+
+    name = error_symbols(name)
+    age = error_symbols(age)
+    tel = error_symbols(tel)
+    mail = error_symbols(mail)
+
+    name = re.search(name_pattern, name)
+    age = re.search(age_pattern, age)
+    tel = re.search(tel_pattern, tel)
+    mail = re.search(mail_pattern, mail)
+
+    print(line)
+    if name is not None:
+        print(name.group(0))
+    else:
+        print("Invalid name")
+
+    if age is not None:
+        print(age.group(0))
+    else:
+        print("Invalid age")
+
+    if tel is not None:
+        print(tel.group(0))
+    else:
+        print("Invalid tel")
+
+    if mail is not None:
+        print(mail.group(0))
+    else:
+        print("Invalid mail")
+
 
 
 
