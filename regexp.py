07ebf49962a766409f8939517527043d7c434fcb
Eklera <lera.googlit@gmail.com>
2020-04-15 23:41:39 +0300

    Add patterns
    Added patterns for name, mail, tel, age

--- regexp.py
+++ regexp.py
@@ -1,2 +1,43 @@
+# Author: Valeriya Timofeeva
+import re
+
+txt = ""
 with open("text.txt", "r", encoding='utf-8') as f:
-    print(f.read())
+    # noinspection PyRedeclaration
+    txt = f.readlines()
+
+
+if not txt:
+    print("File is empty")
+    exit()
+
+name_pattern = re.compile(r"^([А-Я][а-я]*){2,3}")
+age_pattern = re.compile(r"\d+")
+tel_pattern = re.compile(r"\+{0,1}\d{11}")
+mail_pattern = re.compile(r"[\w!#$%&'*+-/=?^_`{|]+[^.@]@(\w+.)+\w+")
+
+def error_symbols(arg):
+    symbols = [".", "@", " "]
+
+
+
+
+
+for line in txt:
+    line = line.split("|")
+    has_empty_elements = False
+    for elem in line:
+        if not elem:
+            has_empty_elements = True
+            break
+
+    if len(line) != 4 or has_empty_elements:
+        continue
+
+    name, age, tel, mail = line
+
+
+
+
+
+
